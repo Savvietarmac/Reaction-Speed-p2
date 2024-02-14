55d5cfd62320ec8e51917962019623cc3132f0de
Savvietarmac Wendel <eric.wendelsal@gmail.com>
2024-02-14 13:54:55 +0000

    change background color of test screens instead of just color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 let time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,] // All the individual test times
 let score = 0 // The eventual score of time/10 for a median of reactionspeed
 let initial = document.getElementById("button-default") // start of test
-initial.addEventListener("click", test)
+let black = document.getElementsByClassName("blackscreen") // black background
+let green = document.getElementsByClassName("greenscreen") // green click screen
+let yellow = document.getElementsByClassName("yellowscreen") // yellow do not click screen
+
+initial.addEventListener("click", e => {
+    setInterval(black, 0, 48, 0)
+    setInterval(green, 0, 4, 0)
+})
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -11,13 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function test() {
-    let black = document.getElementsByClassName("blackscreen")
-    let green = document.getElementsByClassName("greenscreen")
-    let yellow = document.getElementsByClassName("yellowscreen")
-    black.style.display = "visible";
-    if (initial === true) {
-        black.style.display = "visible";
-    }
+
+    black.style.zindex; "2";
     black.style.display = "visible";
     green.style.display = "visible";
     yellow.style.display = "visible";
