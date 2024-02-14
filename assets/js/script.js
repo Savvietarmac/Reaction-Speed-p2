950a2ff72270b7b692e4d1ba862d5770a0582465
Savvietarmac Wendel <eric.wendelsal@gmail.com>
2024-02-14 11:18:01 +0000

    add onclick event to help button, change js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let initial = document.getElementById("button-default")
-    let info = document.getElementById("button-default")
+let time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,] // All the individual test times
+let score = 0 // The eventual score of time/10 for a median of reactionspeed
+let initial = document.getElementById("button-default") // start of test
+initial.addEventListener("click", test)
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    initial.addEventListener("click", test)
 })
 
-function startTest() {
 
-}
 
-function green() {
-
-}
-
-function yellow() {
-
-}
-
-function sequentialTimer() {
-
-}
-
-function storage() {
-
-}
-
-function calcFunction() {
-
-}
-
-function answer() {
-
-}
-
-function highScore() {
-
+function test() {
+    let black = document.getElementsByClassName("blackscreen")
+    let green = document.getElementsByClassName("greenscreen")
+    let yellow = document.getElementsByClassName("yellowscreen")
+    black.style.display = "visible";
+    if (initial === true) {
+        black.style.display = "visible";
+    }
+    black.style.display = "visible";
+    green.style.display = "visible";
+    yellow.style.display = "visible";
+    black.style.display = "none";
+    green.style.display = "none";
+    yellow.style.display = "none";
 }
